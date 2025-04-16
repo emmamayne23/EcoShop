b1bf6fd5c3965c146b003beae221f46ce8b2126b
emmamayne23 <emmamayne23@gmail.com>
2025-04-16 14:54:05 -0400

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+import { FaRegCompass } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaAppStore } from "react-icons/fa";
+import { FaGooglePlay } from "react-icons/fa";
+export const Footer = () => {
+  return (
+    <footer >
+      <div className="p-5 sm:px-10 space-y-10 md:grid md:grid-cols-2 md:gap-5 lg:grid-cols-4">
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center gap-2 text-3xl text-green-400 font-bold">
+          <FaRegCompass />
+          <p className="text-xl">EcoShop</p>
+        </div>
+        <p className="text-gray-400">
+          Your one-stop destination for sustainable and eo-frindly products.
+          Join us in making the world a greener place, one purchase at a time.
+        </p>
+        <div className="flex gap-2 text-gray-400 text-3xl">
+          <FaFacebookF />
+          <FaTwitter />
+          <FaInstagram />
+          <FaYoutube />
+          <FaGithub />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="text-xl font-bold">Quick Links</h2>
+        <ul className="text-gray-300 space-y-2">
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Home
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Shop All
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> New Arrivals
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Bestsellers
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Special Offers
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Gift Cards
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 className="text-xl font-bold">Customer Service</h2>
+        <ul className="text-gray-300 space-y-2">
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Contact Us
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> FAQs
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Shipping Policy
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Returns & Exchanges
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Track Your Order
+          </li>
+          <li className="flex items-center gap-2">
+            <IoIosArrowForward /> Privacy Policy
+          </li>
+        </ul>
+      </div>
+
+      <div className="space-y-3">
+        <h2 className="text-xl font-bold">Stay Connected</h2>
+        <p>
+          Subscribe to our newsletter for the latest products, eco-tips, and
+          exclusive deals.{" "}
+        </p>
+        <form action="" className="flex flex-col gap-3">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your email address"
+            className="focus:outline-none focus:ring-2 focus:ring-green-400 border border-gray-800 p-2 rounded-lg"
+          />
+          <button
+            type="submit"
+            className="bg-orange-500 duration-300 hover:bg-orange-600 p-2 rounded-lg"
+          >
+            Subscribe
+          </button>
+        </form>
+        <div>
+          <p className="text-xl font-bold mb-3">Download Our App</p>
+          <span className="flex gap-5">
+            <p className="hover:bg-gray-800 flex items-center gap-2 p-2 px-5 rounded-lg">
+              <FaAppStore /> IOS
+            </p>
+            <p className="hover:bg-gray-800 flex items-center gap-2 p-2 px-5 rounded-lg">
+              <FaGooglePlay /> Android
+            </p>
+          </span>
+        </div>
+      </div>
+      </div>
+
+      <div className="border-t border-gray-600 flex items-center justify-center">
+        <p>&copy; {new Date().getFullYear()} EcoShop. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { FaRegCompass } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import { CiSearch } from "react-icons/ci";
+import { RiCloseLargeLine } from "react-icons/ri";
+
+import { useState } from "react";
+
+import Link from "next/link";
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <nav className="p-3 pb-0 md:pb-3 px-5 border border-amber-300">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2 text-3xl text-green-400 font-bold">
+          <FaRegCompass />
+          <p className="text-xl">EcoShop</p>
+        </div>
+
+        <ul className="hidden md:flex gap-10">
+          <Link href={"/"} className="hover:text-green-400">Home</Link>
+          <Link href={"/"} className="hover:text-green-400">Shop</Link>
+          <Link href={"/"} className="hover:text-green-400">Categories</Link>
+          <Link href={"/"} className="hover:text-green-400">Deals</Link>
+          <Link href={"/"} className="hover:text-green-400">About</Link>
+        </ul>
+
+        <div className="flex gap-5 text-xl">
+          <FiUser className="hover:cursor-pointer hover:text-green-400" />
+          <span className="relative">
+            <FiShoppingCart className="hover:cursor-pointer hover:text-green-400" />
+            <p className="text-xs bg-orange-500 text-white rounded-full p-1 px-2 flex justify-center items-center absolute -top-3 -right-3">
+              3
+            </p>
+          </span>
+          <button onClick={() => setIsOpen((prevState) => !prevState)}>
+            <FaBars className="md:hidden hover:cursor-pointer hover:text-green-400" />
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center rounded-4xl my-5 h-11 relative bg-gray-800 md:hidden">
+        <CiSearch className="text-2xl text-gray-400 absolute left-3 top-2" />
+        <input
+          type="search"
+          placeholder="Search products..."
+          className="px-5 pl-11 w-full rounded-4xl h-full text-lg focus:ring-2 focus-within:outline-none focus:ring-green-400"
+        />
+      </div>
+      {isOpen && (
+        <ul className="flex flex-col gap-5 bg-gray-800 absolute top-0 right-0 w-4/5 px-10 py-10 pt-24 h-full">
+        <button className="absolute right-10 top-10 hover:cursor-pointer" onClick={() => setIsOpen(prevState => !prevState)}>
+          <RiCloseLargeLine  />
+        </button>
+        <Link href={"/"} className="hover:text-green-400 rounded-xl hover:pl-4 duration-300 hover:bg-gray-900 p-2">Home</Link>
+        <Link href={"/"} className="hover:text-green-400 rounded-xl hover:pl-4 duration-300 hover:bg-gray-900 p-2">Shop</Link>
+        <Link href={"/"} className="hover:text-green-400 rounded-xl hover:pl-4 duration-300 hover:bg-gray-900 p-2">Categories</Link>
+        <Link href={"/"} className="hover:text-green-400 rounded-xl hover:pl-4 duration-300 hover:bg-gray-900 p-2">Deals</Link>
+        <Link href={"/"} className="hover:text-green-400 rounded-xl hover:pl-4 duration-300 hover:bg-gray-900 p-2">About</Link>
+      </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
