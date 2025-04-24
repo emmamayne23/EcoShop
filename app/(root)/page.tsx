26b02d09df603a840534d39cfded43ce5cf164e3
emmamayne23 <emmamayne23@gmail.com>
2025-04-24 09:44:22 -0400

    final one

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,22 +9,23 @@ type Product = {
   image: string;
 };
 
-import { getProducts } from "@/lib/actions/products";
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([])
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const products = await getProducts(); 
-      setProducts(products)
-    }
-
-    fetchProducts()
-  }, [])
+  const fetchProducts = async () => {
+      const res = await fetch("/api/products", { cache: "no-store" });
+      const data = await res.json();
+      setProducts(data);
+    };
+  
+    useEffect(() => {
+      fetchProducts();
+    }, []);
   return (
     <main>
       <Hero />
+      <p className="text-5xl font-bold text-center my-5">Our Products</p>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         { products.map((product) => (
           <ProductCard key={product.id} product={product} />
